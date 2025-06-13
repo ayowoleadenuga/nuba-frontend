05436c6454286cff380265992b2e199d5cae31d2
Elijah <johnnyelijahfemi@gmail.com>
2025-06-13 22:30:42 +0100

    fixed build error

--- redux/features/referralsApiSlice.ts
+++ redux/features/referralsApiSlice.ts
@@ -11,9 +11,11 @@ interface ReferralsParams {
 export const referralsApi = createApi({
   reducerPath: "referralsApi",
   baseQuery: baseQueryWithReauth,
-  endpoints: (builder) => ({
-    getreferrals: builder.query<ReferralsDataResponse, ReferralsParams>({
-      query: ({ startDate, endDate, query }) => {
+  endpoints: builder => ({
+    getreferrals: builder.query<ReferralsDataResponse, ReferralsParams | void>({
+      query: params => {
+        if (!params) return "user/referrals";
+        const { startDate, endDate, query } = params;
         let url = `user/referrals?startDate=${startDate}&endDate=${endDate}`;
         if (query) {
           url += `&query=${encodeURIComponent(query)}`;
